f3c42cf44338a7bed431cd12b094c302d3c1e461
arisukurara <shawncarter896@gmail.com>
2024-11-29 23:44:46 -0500

    Added better labling for the testing output

--- DateClass.cpp
+++ DateClass.cpp
@@ -6,31 +6,46 @@
 int main()
 {
     Date defaultDate; // Create a date object using the default constructor 
+    cout << "Default constructor:" << endl;
     defaultDate.printDate(1); // and display the date using the first date format.
+    cout << endl;
 
     // LeBron James' debut NBA game
+    cout << "Creating date object with parameters, set to Oct 29 2003:" << endl;
     Date parameterDate(10, 29, 2003); // Create a date object using the constructor with parameters 
     parameterDate.printDate(2); // and display the date using the second date format.
+    cout << endl;
 
     // LeBron James' first NBA championship
+    cout << "Testing the set date function, set to Jun 21 2012:" << endl;
     parameterDate.setDate(6, 21, 2012); // Use one of your date objects to test the setDate() function
     parameterDate.printDate(3); // and display the result using the third date format.
+    cout << endl;
 
+    cout << "Date validation:" << endl;
     parameterDate.setDate(13, 45, 2018); // Use setDate() to set the date to 13/45/2018 and verify that this date is not accepted.
     parameterDate.setDate(4, 31, 2000); // Use setDate() to set the date to 4/31/2000 and verify that this date is not accepted.
     parameterDate.setDate(2, 29, 2009); // Use setDate() to set the date to 2/29/2009 and verify that this date is not accepted.
+    cout << endl;
+
 
     defaultDate.setDate(4, 10, 2014); // Set the first date object to 4/10/2014 
     parameterDate.setDate(4, 18, 2014); // and the second date object to 4/18/2014
+    cout << "Testing subtraction" << endl;
+    cout << "4/18/2024 - 4/10/2024: " << endl;
     cout << parameterDate - defaultDate << endl; // subtract the second date from the first date, the result should be 8 days.
+    cout << endl;
 
+    cout << "2/2/2006 - 11/10/2003: " << endl;
     defaultDate.setDate(2, 2, 2006); // Set the first date to 2/2/2006
     parameterDate.setDate(11, 10, 2003); // and the second date to 11/10/2003
     cout << defaultDate - parameterDate << endl; // subtract the second date from the first date, the result should be 815 days.
+    cout << endl;
 
     // Set the date to 2/29/2008, use the pre-decrement operator, and print the date using one of the print formats, and verify that the date is set to 1/31/2008.
     defaultDate.setDate(2, 29, 2008);
     --defaultDate;
+    cout << "Testing decrementing:" << endl;
     cout << endl << "USING PREFIX" << endl;
     defaultDate.printDate(1); // I think Feb 28 should be correct here
 
@@ -45,9 +60,10 @@ int main()
     defaultDate.printDate(1);
     defaultDate++;
     defaultDate.printDate(2);
-
+    cout << endl;
     // Set the date to 12/31/2024, use the post-increment operator, print the date using one of the 
     // print formats, and verify that the date is set to 1 / 1 / 2025.
+    cout << "Testing year changes: " << endl;
     cout << endl << "USING POSTFIX" << endl;
     defaultDate.setDate(12, 31, 2024);
     defaultDate++;
